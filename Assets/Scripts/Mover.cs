4d689dfd94bc7c847053f28d821ec0ec2bfa71f4
Juan Letamendia <juanf03@gmail.com>
2022-11-26 03:51:16 -0300

    Pathfinding with raycast added guided by click to move the player

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -10,6 +10,24 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position;
+        if (Input.GetMouseButtonDown(0))
+        {
+            // if user press left click we discard previous rays to order the user to go to a certain place
+            MoveToCursor();
+        }
+        // starting point is the camera, we make the direction long so it can go far
+        //Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+        //lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInformation;
+        bool hasHit = Physics.Raycast(ray, out hitInformation);
+        if(hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hitInformation.point;
+        }
     }
 }
