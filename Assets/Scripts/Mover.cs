9c37bed000f4580f403b8f6070af1986e9de4fe2
Juan Letamendia <juanf03@gmail.com>
2022-11-27 20:28:55 -0300

    Added character movement and changed angular speed and speed, added UpdateAnimator method to Mover script

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -18,6 +18,8 @@ public class Mover : MonoBehaviour
         // starting point is the camera, we make the direction long so it can go far
         //Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
         //lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        UpdateAnimator();
     }
 
     private void MoveToCursor()
@@ -29,5 +31,14 @@ public class Mover : MonoBehaviour
         {
             GetComponent<NavMeshAgent>().destination = hitInformation.point;
         }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        // our animator only wants to know if we are running or not, that's why we convert from global(world) to local(Player)
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed); 
     }
 }
